c9abb2771761f93cdf66b845c98cdaa4c83b18e3
Kevin Wittek <kevin.wittek@gdata-adan.de>
2017-08-16 10:43:26 +0200

    :tada: Initial commit - yeah!

---
+++ src/main/java/de/gdaag/corp/rp/damnvulnerablespringbootapp/DamnVulnerableSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package de.gdaag.corp.rp.damnvulnerablespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamnVulnerableSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamnVulnerableSpringBootAppApplication.class, args);
+	}
+}
