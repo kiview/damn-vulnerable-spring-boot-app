652f03d648891ef8e710d91ca871bd94987e7ca2
Kevin Wittek <kevin.wittek@styracosoft.de>
2018-12-11 15:04:53 +0100

    Fix psvm

--- src/main/java/com/groovycoder/dvsba/DvsbaApplication.java
+++ src/main/java/com/groovycoder/dvsba/DvsbaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DvsbaApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(DvsbaApplication.class, args);
     }
 
