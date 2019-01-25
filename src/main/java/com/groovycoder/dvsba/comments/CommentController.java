6c08f1552730166612e0fac77547d88fad29fb60
Kevin Wittek <kevin.wittek@codecentric.de>
2019-01-25 12:59:54 +0100

    Fix using immutable list in CommentController

--- src/main/java/com/groovycoder/dvsba/comments/CommentController.java
+++ src/main/java/com/groovycoder/dvsba/comments/CommentController.java
@@ -1,7 +1,5 @@
 package com.groovycoder.dvsba.comments;
 
-import groovy.lang.GroovySystem;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +17,10 @@ import java.util.Map;
 @RequestMapping("/comments")
 public class CommentController {
 
-    private List<String> comments = Arrays.asList("Hi super Webseite leider keine Mobile App deswegen nur einen Stern.", "Ich muss darauf hinweisen, dass eine Webseite ohne Impressum zu betreiben !!!strafbar!!! ist!!!", "Hallo, wir sinds! Wolltest du nicht die Fotos von Samstag hochladen? Grüße!");
+    private List<String> comments = new ArrayList<>(Arrays.asList(
+            "Hi super Webseite leider keine Mobile App deswegen nur einen Stern.",
+            "Ich muss darauf hinweisen, dass eine Webseite ohne Impressum zu betreiben !!!strafbar!!! ist!!!",
+            "Hallo, wir sinds! Wolltest du nicht die Fotos von Samstag hochladen? Grüße!"));
 
     @GetMapping(path = "/")
     public ModelAndView listComments() {
